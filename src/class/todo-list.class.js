68f0d72b9544158e51d6ab4035d19178c2dc2c98
Frank Tineo <frankfelix2016@outlook.es>
2022-10-13 15:45:29 -0400

    clase 108 terminada

---
+++ src/class/todo-list.class.js
@@ -0,0 +1,12 @@
+export class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  nuevoTodo(todo) {
+    this.todos.push(todo);
+  }
+  eliminarTodo(id) {}
+  marcaCompletado(id) {}
+  eliminarCompletados() {}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { saludar } from './js/componentes';
-import './styles.css';
+// import { TodoList } from "./class/todo-list.class";
+// import { Todo } from "./class/todo.class";
+import { TodoList, Todo } from "./class";
+import "./styles.css";
 
-const nombre = 'Fernando';
+const todoList = new TodoList();
+const tarea = new Todo("Aprender Java");
+const tarea2 = new Todo("Comprar Unicornio");
 
-saludar( nombre );
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+console.log(todoList);
